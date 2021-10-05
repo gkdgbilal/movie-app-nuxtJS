7de50a1133af969527ef2d5e0e20572cb630f931
bilal <gkdgbilal>
2021-10-05 12:24:01 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <!-- <h1>This is testing layout</h1> -->
+    <Nuxt keep-alive />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
